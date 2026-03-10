bf70bc31a7044711ba785682ca45bb916eb7be72
Jay Neill <jayneill44@gmail.com>
2026-03-10 10:35:36 -0500

    Add HashTable header file with basic structure

---
+++ DynArr/HashTable.h
@@ -0,0 +1,21 @@
+#ifndef H_HASHTABLE
+#define H_HASHTABLE
+
+const int TABLE_SIZE = 10;
+
+struct Node {
+    int data;
+    Node* next;
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+class HashTable {
+private:
+    Node* hashTable[TABLE_SIZE];
+    int hashFunction(int val);
+public:
+    HashTable();
+    void PrintHashTable();
+};
+
+#endif
