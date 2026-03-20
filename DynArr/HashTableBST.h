137d5c11ac066386a3fc25237b5e523537bc466a
lclark8633 <lclark8633@gmail.com>
2026-03-20 15:40:27 -0500

    fixing binary search

--- DynArr/HashTableBST.h
+++ DynArr/HashTableBST.h
@@ -1,22 +1,25 @@
 #ifndef H_HASHTABLEBST
 #define H_HASHTABLEBST
 
-#include "BST.h"
+#include <vector>
 #include <iostream>
 using namespace std;
 
 class HashTableBST {
 private:
     static const int TABLE_SIZE = 10;
-    BST<int> buckets[TABLE_SIZE];
+    vector<int> buckets[TABLE_SIZE];
+
     int hashFunction(int val);
+    void sortBucket(vector<int>& bucket);
+    bool binarySearch(const vector<int>& bucket, int val);
 
 public:
     HashTableBST();
 
     void insert(int val);
-    bool search(int val);
     bool remove(int val);
+    bool search(int val);
     void print();
 };
 
