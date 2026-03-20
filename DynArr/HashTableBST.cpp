81c410c18fd5336bfa6f1468f771f9c76443f54a
lclark8633 <lclark8633@gmail.com>
2026-03-20 15:43:29 -0500

    fixing binary search
    updated HashTableBST.cpp

--- DynArr/HashTableBST.cpp
+++ DynArr/HashTableBST.cpp
@@ -2,21 +2,20 @@
 
 /**************************************************************
  * Function: HashTableBST::HashTableBST
- * Return: void (constructor)
- * Input:  none
- * Output: Creates a hash table with BST buckets
+ * Return: void
+ * Input: none
+ * Output: Creates hash table with vector buckets
  * author: Liam Clark
  **************************************************************/
 HashTableBST::HashTableBST() {
-    // Nothing needed here since each BST bucket
-    // initializes itself through its own constructor
+    cout << "HashTableBST created with " << TABLE_SIZE << " buckets" << endl;
 }
 
 /**************************************************************
  * Function: HashTableBST::hashFunction
  * Return: int
- * Input:  int val - value to hash
- * Output: Returns the bucket index for the given value
+ * Input: int val
+ * Output: Returns bucket index
  * author: Liam Clark
  **************************************************************/
 int HashTableBST::hashFunction(int val) {
@@ -24,53 +23,111 @@ int HashTableBST::hashFunction(int val) {
 }
 
 /**************************************************************
+ * Function: HashTableBST::sortBucket
+ * Return: void
+ * Input: vector<int>& bucket
+ * Output: Sorts bucket in ascending order
+ * author: Liam Clark
+ **************************************************************/
+void HashTableBST::sortBucket(vector<int>& bucket) {
+    for (int i = 0; i < (int)bucket.size() - 1; i++) {
+        for (int j = 0; j < (int)bucket.size() - 1 - i; j++) {
+            if (bucket[j] > bucket[j + 1]) {
+                int temp = bucket[j];
+                bucket[j] = bucket[j + 1];
+                bucket[j + 1] = temp;
+            }
+        }
+    }
+}
+
+/**************************************************************
+ * Function: HashTableBST::binarySearch
+ * Return: bool
+ * Input: const vector<int>& bucket, int val
+ * Output: Returns true if found, false otherwise
+ * author: Liam Clark
+ **************************************************************/
+bool HashTableBST::binarySearch(const vector<int>& bucket, int val) {
+    int left = 0;
+    int right = (int)bucket.size() - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (bucket[mid] == val)
+            return true;
+        else if (val < bucket[mid])
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+
+    return false;
+}
+
+/**************************************************************
  * Function: HashTableBST::insert
  * Return: void
- * Input:  int val - value to insert
- * Output: Inserts the value into the correct BST bucket
+ * Input: int val
+ * Output: Inserts into correct bucket
  * author: Liam Clark
  **************************************************************/
 void HashTableBST::insert(int val) {
     int index = hashFunction(val);
-    buckets[index].Insert(val);
-}
-
-/**************************************************************
- * Function: HashTableBST::search
- * Return: bool
- * Input:  int val - value to search for
- * Output: Returns true if the value is found, false otherwise
- * author: Liam Clark
- **************************************************************/
-bool HashTableBST::search(int val) {
-    int index = hashFunction(val);
-    return buckets[index].Search(val);
+    buckets[index].push_back(val);
 }
 
 /**************************************************************
  * Function: HashTableBST::remove
  * Return: bool
- * Input:  int val - value to remove
- * Output: Removes the value from the correct BST bucket
- *         and returns true if successful, false otherwise
+ * Input: int val
+ * Output: Removes value if found
  * author: Liam Clark
  **************************************************************/
 bool HashTableBST::remove(int val) {
     int index = hashFunction(val);
-    return buckets[index].Remove(val);
+    sortBucket(buckets[index]);
+
+    for (int i = 0; i < (int)buckets[index].size(); i++) {
+        if (buckets[index][i] == val) {
+            buckets[index].erase(buckets[index].begin() + i);
+            return true;
+        }
+    }
+    return false;
+}
+
+/**************************************************************
+ * Function: HashTableBST::search
+ * Return: bool
+ * Input: int val
+ * Output: Sorts bucket, then binary searches it
+ * author: Liam Clark
+ **************************************************************/
+bool HashTableBST::search(int val) {
+    int index = hashFunction(val);
+    sortBucket(buckets[index]);
+    return binarySearch(buckets[index], val);
 }
 
 /**************************************************************
  * Function: HashTableBST::print
  * Return: void
- * Input:  none
- * Output: Prints all buckets and their BST contents
+ * Input: none
+ * Output: Prints all buckets
  * author: Liam Clark
  **************************************************************/
 void HashTableBST::print() {
-    for (int i = 0; i < TABLE_SIZE; ++i) {
-        cout << "Bucket [" << i << "] : ";
-        buckets[i].Print();
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        cout << "Bucket [" << i << "]: ";
+        if (buckets[i].empty()) {
+            cout << "empty";
+        } else {
+            for (int j = 0; j < (int)buckets[i].size(); j++) {
+                cout << buckets[i][j] << " ";
+            }
+        }
         cout << endl;
     }
 }
