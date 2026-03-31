74f01937a6a7e57864e10b51e70bfe037f25caba
Jay Neill <jayneill44@gmail.com>
2026-03-31 10:37:00 -0500

    Refactor hash table size and include statements

--- DynArr/HashTableLL.h
+++ DynArr/HashTableLL.h
@@ -2,20 +2,26 @@
 #define H_LLHASH
 #include "LinkedList.h"
 
-const int TABLE_SIZE = 10;
+#include "Linkedlist.h"
+
+const int LL_TABLE_SIZE = 10;
+
+
 
 class HashTableLL {
 private:
-    LinkedList<int> hashTable[TABLE_SIZE];
+    LinkedList<int> hashTable[LL_TABLE_SIZE];
     int hashFunction(int val);
 public:
     HashTableLL();
     ~HashTableLL();
+    void insert(int val);
+    bool remove(int val);
+
 
     void insert(int val);
     void remove(int val);
     bool HashSearch(int val);
-
     void PrintHashTable();
 };
 
